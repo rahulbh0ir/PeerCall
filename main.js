1aae99559a7f4ceb935146f236381216ea7360d7
Rahul Bhoir <rahulbhoir767676@gmail.com>
2025-08-05 14:28:15 +0530

    up

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+
+let App_ID = "ff28096ad7cd4e6b9fdfe6f11787013d";
+
+let token = null
+let uid = String(Math.floor(Math.random() * 10000));
+
+let client;
+let channel;
+
 let localStream;
 let remoteStream;
 let peerConnection;
@@ -13,6 +22,7 @@ const servers = {
 
 
 let main = async () => {
+
   localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false })
   document.getElementById("user-1").srcObject = localStream  
 
@@ -27,12 +37,32 @@ let createOffer = async () => {
   remoteStream = new MediaStream()
   document.getElementById("user-2").srcObject = remoteStream  
 
-  let a = localStream.getTracks()
+
+  localStream.getTracks().forEach((track) => {
+    peerConnection.addTrack(track, localStream)
+  })
+
+  peerConnection.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track)
+    })
+  }
+
+  peerConnection.onicecandidate = (event) => {
+    if(event.candidate) {
+      console.log("New ICE candidate : ", event.candidate);
+    }
+  }
+
+
+
 
   let offer = await peerConnection.createOffer();
+  console.log("Offer created: ", offer);
+
   await peerConnection.setLocalDescription(offer)
 
-  console.log(a)
+
 }
 
 
