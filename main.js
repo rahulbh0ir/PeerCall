67a1f40e0deddf87d967a9f00807f589e240280d
Rahul Bhoir <rahulbhoir767676@gmail.com>
2025-09-30 20:31:45 +0530

    up

--- main.js
+++ main.js
@@ -2,23 +2,31 @@
 let local;
 
 
-let video = document.getElementById("video")
-let info = document.getElementById("info")
+let video = document.getElementById("video");
+let info = document.getElementById("info");
 
-let localVideo = document.getElementById("user-1")
-let remoteVideo = document.getElementById("user-2")
+let localVideo = document.getElementById("user-1");
+let remoteVideo = document.getElementById("user-2");
 
 
 
 
 async function join() {
   
+  let room = document.getElementById("room").value.trim();
+  console.log(room);
+  
+  if(!room) {
+    alert("Please enter a room name");
+    return;
+  }
+
   // video.style.display = "block"
   // info.style.display = "none"
-
-  let stream = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
-  localVideo.srcObject = stream;
   
+
+  let stream = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
+  localVideo.srcObject = stream;
   
 
 
